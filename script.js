3fe5c3800069a8d82bf7189171714d9f10123864
HawkSWE <hook200364@gmail.com>
2022-08-24 13:36:59 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+const name = document.getElementById('name')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const conpass = document.getElementById('confirm_password')
+const form = document.getElementById('form')
+const fel = document.getElementById('fel')
+
+form.addEventListener('submit', (e) => {
+  let messages = []
+  if (name.value === '' || name.value == null) {
+    messages.push('Name is required')
+  }
+
+  if (email.value === '' || email.value == null) {
+    messages.push('Email is required')
+  }
+
+  if (password.value === '' || password.value == null) {
+    messages.push('Password is required')
+  }
+
+  if (password.value.length < 6) {
+    messages.push('Password must be atleast six characters')
+  }
+
+  if (conpass.value === '' || conpass.value == null) {
+    messages.push('Confirm password is required')
+  }
+
+  if (password.value !== conpass.value){
+    messages.push('Password and confirm password need to be the same')
+  }
+
+  if (messages.length > 0) {
+    e.preventDefault()
+    fel.innerText = messages.join(', ')
+  }
+
+})
